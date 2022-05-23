97269e167af7382d435c4bc42178b522f888fca7
lejun <lejun3389@qq.com>
2022-05-23 21:51:25 +0800

    二分法获取第k个数

---
+++ src/main/java/com/example/leetcode/daily/Leet0518.java
@@ -0,0 +1,27 @@
+package com.example.leetcode.daily;
+
+public class Leet0518 {
+    /**
+     * 含义就是得到乘法表中小于等于 x 的数字个数恰好为 k 时， x的值。
+     * @param m
+     * @param n
+     * @param k
+     * @return
+     */
+    public int findKthNumber(int m, int n, int k) {
+        int left = 1, right = m * n;
+        while (left < right) {
+            int x = left + (right - left) / 2;
+            int count = x / n * n;
+            for (int i = x / n + 1; i <= m; ++i) {
+                count += x / i;
+            }
+            if (count >= k) {
+                right = x;
+            } else {
+                left = x + 1;
+            }
+        }
+        return left;
+    }
+}
